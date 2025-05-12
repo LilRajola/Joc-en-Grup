31e0dd11a93cde3f2c6d06b2b09079139ca7aff3
AuraRizz <205314701+AuraRizz@users.noreply.github.com>
2025-05-12 21:34:27 +0200

    Arreglat enemics
     s'ha acabat la desolació, enemics arreglats i eliminats els tres inicials

--- joc-en-grup/Scripts/creació_enemics.gd
+++ joc-en-grup/Scripts/creació_enemics.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
+@onready var nau = $"../Nau"
 @onready var timer: Timer = $Timer
 @export var escena_enemic: PackedScene
 var enemics_creats := 0
 var maxim_enemics := 20
+var distancia = 100
+
 
 func _ready() -> void:
-	timer.wait_time = 1.0
+	timer.wait_time = 0.5
 	timer.one_shot = false
 	timer.autostart = true
 	timer.timeout.connect(_on_timer_timeout)
@@ -15,7 +18,8 @@ func _on_timer_timeout() -> void:
 	if enemics_creats < maxim_enemics:
 		var nou_enemic = escena_enemic.instantiate()
 		add_child(nou_enemic)
-		nou_enemic.position = Vector2(100,0)  # o posició concreta
+		print("nou_enemic")
+		nou_enemic.position = Vector2(nau.position.x + distancia,-165)
 		enemics_creats += 1
 	else:
 		timer.stop()
