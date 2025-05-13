d291ba406c86958725e1eaa11995e9836d81a475
ERICERIC-jpg <Balayla.ceres.alex@alumnes.fundaciocic.org>
2025-05-13 09:57:17 +0200

    Button Audio
    botó d'audio menú

--- joc-en-grup/Scripts/menu.gd
+++ joc-en-grup/Scripts/menu.gd
@@ -1,6 +1,28 @@
 extends Node2D
 
+var ON := true
+func _ready():
+	$Audio/AudioStreamPlayer.play()
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Escenes/mapa_2d.tscn")
+
+
+func _on_audio_pressed():
+	ON = !ON 
+	if ON:
+		$Audio/AudioStreamPlayer.play()
+	else:
+		$Audio/AudioStreamPlayer.stop()
+
+
+func _on_audio_stream_player_finished():
+	$Audio/AudioStreamPlayer.play()
+
+
+
+
+	
+
+
 	
